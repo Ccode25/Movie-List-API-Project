f2d7d0ee61a3e7a5d6c87ceba41f23b5798c1450
Ccode25 <ajdaelo25@gmail.com>
2024-08-11 10:13:01 +0800

    initial commit

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
 
-const app = express()
+const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.get("/home", (req, res) => {
-  res.render("index.html", {content: "Waiting for content" })
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
 })
 
 
